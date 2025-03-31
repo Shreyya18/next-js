56fb4d4617701bb26b6cfc60dbec22533dddef0e
shreyya18 <shreyapoojary251@gmail.com>
2025-03-31 11:49:22 +0530

    add user profile page and update users dashboard with links

--- next-app/app/dashboard/users/page.tsx
+++ next-app/app/dashboard/users/page.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
-
+import React from "react";
+import Link from "next/link";
 const page = () => {
   return (
-    <div>users</div>
-  )
-}
+    <div>
+      <h1>Dashboard Users</h1>
 
-export default page
+      <ul className="mt-10">
+        <li>
+          <Link href="/dashboard/users/1">user 1</Link>
+        </li>
+        <li>
+          <Link href="/dashboard/users/2">user 2</Link>
+        </li>
+        <li>
+          <Link href="/dashboard/users/3">user 3</Link>
+        </li>
+        <li>
+          <Link href="/dashboard/users/4">user 4</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default page;
