31e6921b1584877797b43fd5255a26c2c4311078
shreyya18 <shreyapoojary251@gmail.com>
2025-03-31 15:29:25 +0530

    add product review and review list components

---
+++ routing-demo/src/app/product/[productId]/reviews/page.tsx
@@ -0,0 +1,11 @@
+export default function ReviewList(){
+    return(
+        <>
+        <h1>Review List</h1>
+        <li>Best</li>
+        <li>Better</li>
+        <li>Good</li>
+        <li>Bad</li>
+        </>
+    )
+}
