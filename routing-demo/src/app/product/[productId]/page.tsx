9028d2b96511aa18a9236ccb233775ff329985df
shreyya18 <shreyapoojary251@gmail.com>
2025-04-13 23:10:12 +0530

    initail commit

--- routing-demo/src/app/product/[productId]/page.tsx
+++
@@ -1,9 +0,0 @@
- export default  async function ProductDetails({params,  
-    }:{
-        params:Promise<{productId:string}>; 
-    }) {
-    const productId=(await params).productId;
-    return (
-        <h1>Detail about Product {productId}</h1>
-    )
-}
